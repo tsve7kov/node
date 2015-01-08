4990953470c7868b67e39ab27e28af939403a02b
Eugene Tsvetkov <tsve7kov@gmail.com>
2015-01-07 19:31:09 -0800

    new repo

---
+++ node.js
@@ -0,0 +1,7 @@
+var http = require("http");
+
+http.createServer(function(request, response){
+	response.writeHead(200,{"Content-Type": "text/plain"});
+	response.write("test");
+	response.end();
+}).listen(8888);
---
+++ server.js
@@ -0,0 +1,27 @@
+http = require("http");
+url = require("url");
+
+function start(route, handle) {
+	function onRequest(request, response) {
+		var postData = "";
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		request.setEncoding("utf8");
+		request.addListener("data", function (postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '" +
+				postDataChunk + "'.");
+		});
+
+		request.addListener("end", function () {
+			console.log("complete post data is: " + postData)
+			route(handle, pathname, response, postData);
+		});
+		//var content = route(handle, pathname, response);
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started");
+}
+
+exports.start = start;
